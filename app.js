54ec8b204b784194482bf189fc3997af4830e298
luke-h1 <luke.howsam@yahoo.com>
2020-09-02 11:22:22 +0100

    add song preview and card elements to app.js

--- app.js
+++ app.js
@@ -11,24 +11,67 @@ async function getSongs(term) {
   showData(data);
 }
 
-// getSongs('drake')
+async function songPaginationFetch(url) {
+  const MOD_URL = `https://cors-anywhere.herokuapp.com/${url}`;
+  const res = await fetch(MOD_URL);
+  const data = await res.json();
+  showData(data);
+}
 
 function showData(data) {
   let output = '';
   data.data.forEach((song) => {
-    output += `
-    <ul class="songs">
-    <li><span><strong>${song.artist.name}</strong> - ${song.title}</span>
-    <div class="btn-container"> 
-    <button class="btn" data-artist=${song.artist.name} data-songtitle="${song.title}">Get Lyrics </button> 
-    </div> 
-    </li> 
-    </ul>
-        `;
+    output += ` 
+    <div class="grid-container"> 
+      <div class="card"> 
+          <h6 class="card-title">${song.artist.name}</strong> - ${song.title}</h6> 
+          <div class="list-item"> 
+          <img src="${song.album.cover}" class="card-image"/> 
+          <ul class="songs">
+              <button class="btn" data-artist=${song.artist.name} data-songtitle="${song.title}">Get Lyrics </button> 
+            </ul>  
+            <div class="preview-container"> 
+            <audio controls>
+            <source src="${song.preview}" type="audio/ogg">
+          </audio> 
+          </div> 
+          </div> 
+          </div> 
+          </div> 
+
+    `;
+    
+    if (data.prev || data.next) {
+      pagination.innerHTML = `
+  ${
+    data.prev
+      ? `<button class="btn" onClick="songPaginationFetch(${data.prev})">Previous</button`
+      : ''
+  } 
+  ${
+    data.next
+      ? `<button class="btn" onClick="songPaginationFetch(${data.next})">Next</button`
+      : ''
+  }
+      `;
+    } else {
+      pagination.innerHTML = '';
+    }
   });
   result.innerHTML = `<ul>${output}</ul>`;
 }
 
+async function getLyrics(artist, title) {
+  const res = await fetch(`${API_URL}/v1/${artist}/${title}`);
+  const data = await res.json();
+  const lyrics = data.lyrics.replace(/(\r\n|\r|\n)/g, '<br>');
+  result.innerHTML = `
+  <h2><strong>${artist}</strong> - ${title}</h2> 
+  <span>${lyrics}</span>`;
+  pagination.innerHTML = '';
+}
+
+// EVENT LISTENERS
 form.addEventListener('submit', (e) => {
   e.preventDefault();
   const searchTerm = query.value.trim();
@@ -36,5 +79,16 @@ form.addEventListener('submit', (e) => {
     console.log('123456');
   } else {
     getSongs(searchTerm);
+  }
+});
+
+// get song lyric button
+result.addEventListener('click', (e) => {
+  console.log(e.target);
+  const clickedEl = e.target;
+  if (clickedEl.tagName === 'BUTTON') {
+    const artist = clickedEl.getAttribute('data-artist');
+    const title = clickedEl.getAttribute('data-songtitle');
+    getLyrics(artist, title);
   }
 });
