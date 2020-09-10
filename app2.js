d126b1c1f09bf3dcab8deb6821b9b786cadb6974
luke-h1 <luke.howsam@yahoo.com>
2020-09-10 13:43:42 +0100

    refactor

--- app2.js
+++ app2.js
@@ -1,21 +1,17 @@
-const form = document.getElementById('form'); 
+const form = document.getElementById('form');
 const query = document.getElementById('query');
 const result = document.getElementById('results');
 const errorEl = document.getElementById('error');
-const API_URL = `https://theaudiodb.com/api/v1/json/1/search.php?s=`; 
+const API_URL = `https://theaudiodb.com/api/v1/json/1/search.php?s=`;
 
-
-
-async function getData(term){
-  const res = await fetch(`${API_URL}${term}`)
+async function getData(term) {
+  const res = await fetch(`${API_URL}${term}`);
   const data = await res.json();
   console.log(data);
   showDataDOM(data);
 }
 
-
-
-function showDataDOM(data){
+function showDataDOM(data) {
   let output = '';
   data.artists.forEach((artist) => {
     output += ` 
@@ -36,31 +32,19 @@ function showDataDOM(data){
         </ul>           
         </div>
         <div class="card-action">
-          <a href="#">This is a link</a>
         </div>
       </div>
     </div>
     </div> 
     </div> 
     
-    `
-  })
-  result.innerHTML = `${output}`
+    `;
+  });
+  result.innerHTML = `${output}`;
 }
 
-
-
-
 form.addEventListener('submit', (e) => {
-  e.preventDefault(); 
+  e.preventDefault();
   const searchTerm = query.value.trim();
   getData(searchTerm);
-})
-
-
-
-
-
-
-
-      
+});
